9543bc1fe1460f91753a3e312b678271c621f060
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-10-13 20:20:11 +0530

    fix : Error Handeling done

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var btnTranslate = document.querySelector(".translateButton");
 
 var translatedTextArea = document.querySelector(".translatedTextArea");
 
+var errorMsgValue = document.querySelector(".errorMsg");
 var Url = "https://api.funtranslations.com/translate/ferb-latin.json";
 
 function getTranslationURL(text) {
@@ -12,6 +13,10 @@ function getTranslationURL(text) {
 
 function clickHandler() {
   var userInput = textAreaToTranslate.value;
+  console.log(userInput);
+  if (userInput == "") {
+    errorMsgValue.innerText = "Please Enter Text To Translate";
+  }
   fetch(getTranslationURL(userInput))
     .then((response) => response.json())
     .then((json) => {
